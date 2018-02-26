62ba7d8ee2e3ffab2f048824dd074d6b2af626cb
Vojtech Hrdina <vhrdina@gmail.com>
2018-02-26 01:27:33 +0000

    Api parameters, Mock response

--- app/src/main/java/cz/mangoweb/appstore/api/BoostApi.kt
+++ app/src/main/java/cz/mangoweb/appstore/api/BoostApi.kt
@@ -2,29 +2,48 @@ package cz.mangoweb.appstore.api
 
 import cz.mangoweb.appstore.api.model.*
 import io.reactivex.Observable
+import retrofit2.http.*
 
 interface BoostApi {
 
-    fun auth(auth: AuthRequest): Observable<AuthResponse>
+    @POST("auth")
+    fun auth(@Body auth: AuthRequest): Observable<AuthResponse>
 
-    fun refreshToken(): Observable<RefreshTokenResponse>
+    @POST("token")
+    fun refreshToken(@Body token: RefreshTokenRequest): Observable<RefreshTokenResponse>
 
+    @GET("teams")
     fun getTeams(): Observable<List<Team>>
 
-    fun createTeam(team: CreateTeamRequest): Observable<Team>
+    @POST("teams")
+    fun createTeam(@Body team: CreateTeamRequest): Observable<Team>
 
-    fun checkTeam(team: TeamCheckRequest): Observable<TeamCheckResponse>
+    @POST("teams/check")
+    fun checkTeam(@Body team: TeamCheckRequest): Observable<TeamCheckResponse>
 
-    fun getTeam(): Observable<Team>
+    @GET("teams/{id}")
+    fun getTeam(@Path("id") id: Int): Observable<Team>
 
-    fun updateTeam(team: Team): Observable<Team>
+    @PUT("teams/{id}")
+    fun updateTeam(@Path("id") id: Int, @Body team: Team): Observable<Team>
 
-    fun filter(): Observable<List<App>>
+    @GET("overview?sort={name}&filter={filter}&platform={platform}")
+    fun filter(@Path("") name: String, @Path("filter") filter: String, @Path("platform") platform: String): Observable<List<App>>
 
-    fun upload(): Observable<App>
+    @Multipart
+    @POST("upload?tags={tags}")
+    fun upload(@Path("tags") tags: String): Observable<App>
 
-    fun getApps(): Observable<List<App>>
+    @GET("apps/{platform}/{identifier}")
+    fun getApps(@Path("platform") platform: String, @Path("identifier") identifier: String): Observable<List<App>>
 
-    fun getApp(): Observable<App>
+    @GET("apps/{id}?depth={level}")
+    fun getApp(@Path("id") id: String, @Path("level") level: Int): Observable<App>
+
+    @GET("apps/{id}/auth")
+    fun getDownloadToken(@Path("id") id: Int): Observable<AppTokenResponse>
+
+    @GET("apps/{id}/file?download={token}")
+    fun downloadApp(@Path("id") id: Int, @Path("token") token: String)
 
 }
---
+++ app/src/main/java/cz/mangoweb/appstore/api/model/RefreshTokenRequest.kt
@@ -0,0 +1,8 @@
+package cz.mangoweb.appstore.api.model
+
+/**
+ * {
+ * "token": "2018-01-10T18:38:35.762Z"
+ * }
+ */
+data class RefreshTokenRequest(val token: String)
