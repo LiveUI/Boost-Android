8fbc6a4a8204f4390618668740e282b8c53ccabf
Vojtech Hrdina <vhrdina@gmail.com>
2018-03-09 04:16:43 +0000

    MockResponseResolver - Fix

--- app/src/mock/java/cz/mangoweb/appstore/api/MockResponseResolver.kt
+++ app/src/mock/java/cz/mangoweb/appstore/api/MockResponseResolver.kt
@@ -1,15 +1,9 @@
 package cz.mangoweb.appstore.api
 
 import android.app.Application
-import cz.mangoweb.appstore.AppStoreApplication
 import cz.mangoweb.appstore.BuildConfig
-import io.reactivex.Single
 import okhttp3.Request
-import okhttp3.RequestBody
-import okio.Buffer
 import org.apache.commons.io.IOUtils
-import timber.log.Timber
-import java.io.EOFException
 import java.io.IOException
 import java.io.InputStream
 import java.nio.charset.Charset
@@ -20,7 +14,7 @@ import javax.inject.Inject
  * Created by Vojtech Hrdina on 05/03/2018.
  */
 
-class MockResponseResolver @Inject constructor(val application: Application) {
+class MockResponseResolver @Inject constructor(private val application: Application) {
 
     private var rules: Array<UrlRule>
 
@@ -47,7 +41,7 @@ class MockResponseResolver @Inject constructor(val application: Application) {
 
     @Throws(IOException::class)
     fun getMockResponse(request: Request): String {
-        val url: String = request.url().toString().replace(BuildConfig.BASE_URL, "")
+        val url: String = request.url().toString()
         val method = request.method()
         val path: String = normalizeUrl(url, method)
 
@@ -56,17 +50,20 @@ class MockResponseResolver @Inject constructor(val application: Application) {
 
     internal class UrlRule(val pattern: String, val replace: String, val pattern2: String? = null)
 
-    private fun normalizeUrl(url: String, method: String): String {
+    fun normalizeUrl(url: String, method: String): String {
+
+        var urlOpt = url.replace(BuildConfig.BASE_URL, "")
+
         for (rule in rules) {
             if(rule.pattern2 == null) {
-                url.replace(rule.pattern, rule.replace)
+                urlOpt = urlOpt.replace(rule.pattern, rule.replace)
             } else {
                 if(Pattern.matches(rule.pattern2, url)) {
-                    url.replace(rule.pattern, rule.replace)
+                    urlOpt = urlOpt.replace(rule.pattern, rule.replace)
                 }
             }
         }
-        return "$method/$url.json".replace("?", "_")
+        return "$method/$urlOpt.json".replace("?", "_")
     }
 
 }
---
+++ app/src/test/java/cz/mangoweb/appstore/api/MockResponseResolverTest.kt
@@ -0,0 +1,14 @@
+package cz.mangoweb.appstore.api
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MockResponseResolverTest {
+
+    @Test
+    fun normalizeUrl() {
+
+    }
+
+}
